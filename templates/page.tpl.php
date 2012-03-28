d5d57124f1e7db84aedfb52876ab75d77759f8af
Trude Leknes <trudeleknes@gmail.com>
2012-03-28 12:23:13 +0200

    Diverse css + logo 	modified:   css/blocks.css 	modified:   css/fields.css 	modified:   css/html-reset.css 	modified:   css/layout-fixed.css 	new file:   images/logo2.png 	modified:   logo.png 	modified:   templates/page.tpl.php

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -82,7 +82,7 @@
         <?php if ($site_name): ?>
           <?php if ($title): ?>
             <div id="site-name"><strong>
-              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+             <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
             </strong></div>
           <?php else: /* Use h1 when the content title is empty */ ?>
             <h1 id="site-name">
